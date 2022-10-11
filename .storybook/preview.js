a111d9bb2c2edf9dbdbcc4ccf1dfb69e6f8528d3
Matheus <matheuscruzhen@gmail.com>
2022-10-11 17:06:53 -0300

    add mock service worker

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,12 @@
+import { themes } from '@storybook/theming';
+import { initialize, mswDecorator } from 'msw-storybook-addon';
 import '../src/global.css';
+
+// Initialize MSW
+initialize();
+
+// Provide the MSW addon decorator globally
+export const decorators = [mswDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -7,5 +15,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  docs: {
+    theme: themes.dark,
   },
 };
